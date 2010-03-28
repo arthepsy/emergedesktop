0c9b4f4265d3777344435733f31b5839cdc5b56b
Chris Sutcliffe <ir0nh34d@gmail.com>
2010-03-27 22:41:49 -0400

    Add emergeIcons

---
+++ Source/emergeIcons/resource.h
@@ -0,0 +1,36 @@
+//---
+//
+//  This file is part of Emerge Desktop.
+//  Copyright (C) 2004-2007  The Emerge Desktop Development Team
+//
+//  Emerge Desktop is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Emerge Desktop is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//---
+
+#include <winver.h>
+
+#define IDI_EMERGE  0
+#define IDI_ABORT   1
+#define IDI_ADD     2
+#define IDI_DELETE  3
+#define IDI_DOWN    4
+#define IDI_EDIT    5
+#define IDI_FOLDER  6
+#define IDI_GATHER  7
+#define IDI_INFO    8
+#define IDI_SAVE    9
+#define IDI_SAVEAS  10
+#define IDI_START   11
+#define IDI_STOP    12
+#define IDI_UP      13
