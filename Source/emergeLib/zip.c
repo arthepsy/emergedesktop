ce3fa3205566e3c2bc3a2af90007cd3bbd41dc2b
Chris Sutcliffe <ir0nh34d@gmail.com>
2010-07-31 23:19:16 -0400

    Code clean-up

--- Source/emergeLib/zip.c
+++ Source/emergeLib/zip.c
@@ -1,20 +1,27 @@
-/*
-   Copyright (c) 1990-2010 Info-ZIP.  All rights reserved.
+//----  --------------------------------------------------------------------------------------------------------
+//
+//  This file is part of Emerge Desktop.
+//  Copyright (C) 2004-2010  The Emerge Desktop Development Team
+//
+//  Emerge Desktop is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Emerge Desktop is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//----  --------------------------------------------------------------------------------------------------------
+//
+// Based on example.c included as part of Info-zip
+//
+//----  --------------------------------------------------------------------------------------------------------
 
-   See the accompanying file LICENSE, version 2009-Jan-02 or later
-   (the contents of which are also included in zip.h) for terms of use.
-   If, for some reason, all these files are missing, the Info-ZIP license
-   also may be found at:  ftp://ftp.info-zip.org/pub/infozip/license.html
-   */
-/*
-   A very simplistic example of how to load the zip dll and make a call into it.
-   Note that none of the command line options are implemented in this example.
-
-*/
-
-#ifndef WIN32
-#  define WIN32
-#endif
 #define API
 
 /* Tell Microsoft Visual C++ 2005 to leave us alone and
@@ -29,136 +36,55 @@
 #  endif
 #endif
 
-/*#include <stdlib.h>
-#include <stdio.h>
-#include <commdlg.h>
-#include <dlgs.h>*/
-
-#ifndef EXPENTRY
-#define EXPENTRY WINAPI
-#endif
-
-#include "../zip/windll/structs.h"
-
-/* Defines */
-#ifndef MSWIN
-#define MSWIN
-#endif
-
-typedef int (WINAPI * _DLL_ZIP)(ZCL, LPZPOPT);
-typedef int (WINAPI * _ZIP_USER_FUNCTIONS)(LPZIPUSERFUNCTIONS);
-
-/* Global variables */
-
-extern LPZIPUSERFUNCTIONS lpZipUserFunctions;
-
-extern HINSTANCE hZipDll;
-
-extern int hFile;                 /* file handle             */
-
-/* Global functions */
-
-extern int WINAPI ZpArchive(ZCL C, LPZPOPT Opts);
-extern int WINAPI ZpInit(LPZIPUSERFUNCTIONS lpZipUserFunc);
-
-#include <assert.h>    /* required for all Windows applications */
-#include <windowsx.h>
-#include <windows.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <string.h>
-#ifdef __BORLANDC__
-#include <dir.h>
-#else
-#include <direct.h>
-#endif
-#include "zip.h"
-#include "../zip/revision.h"
-/* printf get redirected by a macro define in api.h !!! */
-#ifdef printf
-# undef printf
-#endif
-
-#ifdef WIN32
-#include <commctrl.h>
-#include <winver.h>
-#else
-#include <ver.h>
-#endif
-
 #ifdef __GNUC__
 #define UNUSED __attribute__((unused))
 #else
 #define UNUSED
 #endif
 
-#ifdef WIN32
-#define ZIP_DLL_NAME "ZIP32Z64.DLL\0"
-#else
-#define ZIP_DLL_NAME "ZIP16.DLL\0"
+#include "../zip/windll/structs.h"
+#include <windows.h>
+#include "zip.h"
+#include "../zip/revision.h"
+/* printf get redirected by a macro define in api.h !!! */
+#ifdef printf
+#undef printf
 #endif
 
-#define DLL_WARNING "Cannot find %s."\
-  " The Dll must be in the application directory, the path, "\
-"the Windows directory or the Windows System directory."
-#define DLL_VERSION_WARNING "%s has the wrong version number."\
-  " Insure that you have the correct dll's installed, and that "\
-"an older dll is not in your path or Windows System directory."
+/* Global variables */
 
-int hFile;              /* file handle */
-
+extern LPZIPUSERFUNCTIONS lpZipUserFunctions;
 ZCL ZpZCL;
 LPZIPUSERFUNCTIONS lpZipUserFunctions;
 HANDLE hZUF = (HANDLE)NULL;
-HINSTANCE hUnzipDll;
 HANDLE hFileList;
 ZPOPT ZpOpt;
-#ifdef WIN32
-DWORD dwPlatformId = 0xFFFFFFFF;
-#endif
-HINSTANCE hZipDll;
+
+/* Global functions */
+
+extern int WINAPI ZpArchive(ZCL C, LPZPOPT Opts);
+extern int WINAPI ZpInit(LPZIPUSERFUNCTIONS lpZipUserFunc);
 
 /* Forward References */
-_DLL_ZIP ZipArchive;
-_ZIP_USER_FUNCTIONS ZipInit;
 
 void FreeUpMemory(void);
 int WINAPI DummyPassword(LPSTR, int, LPCSTR, LPCSTR);
 int WINAPI DummyPrint(char far *, unsigned long);
 int WINAPI WINAPI DummyComment(char far *);
 
-#ifdef WIN32
-BOOL IsNT(VOID);
-#endif
-
 /****************************************************************************
 
 FUNCTION: Main(int argc, char **argv)
 
  ****************************************************************************/
-#ifdef __BORLANDC__
-#  ifdef WIN32
-#pragma argsused
-#  endif
-#endif
 int MakeZip(char *filename, char *root, char *path)
 {
   /*char szFileList[1][MAX_PATH];
     char testPath[MAX_PATH];*/
-  LPSTR szFileList;
+  LPSTR szFileList = NULL;
   char **index, *sz;
   int retcode, i, cc;
-  DWORD dwVerInfoSize;
-  DWORD dwVerHnd;
   char szFullPath[PATH_MAX];
-#ifdef WIN32
-  char *ptr;
-#else
-  HFILE hfile;
-  OFSTRUCT ofs;
-#endif
-  HANDLE  hMem;         /* handle to mem alloc'ed */
 
   hZUF = GlobalAlloc( GPTR, (DWORD)sizeof(ZIPUSERFUNCTIONS));
   if (!hZUF)
@@ -270,7 +196,6 @@ int MakeZip(char *filename, char *root, char *path)
   GlobalUnlock(hFileList);
   GlobalFree(hFileList);
   FreeUpMemory();
-  FreeLibrary(hZipDll);
   return 1;
 }
 
@@ -282,28 +207,6 @@ void FreeUpMemory(void)
       GlobalFree(hZUF);
     }
 }
-
-#ifdef WIN32
-/* This simply determines if we are running on NT */
-BOOL IsNT(VOID)
-{
-  if(dwPlatformId != 0xFFFFFFFF)
-    return dwPlatformId;
-  else
-    /* note: GetVersionEx() doesn't exist on WinNT 3.1 */
-    {
-      if(GetVersion() < 0x80000000)
-        {
-          dwPlatformId = TRUE;
-        }
-      else
-        {
-          dwPlatformId = FALSE;
-        }
-    }
-  return dwPlatformId;
-}
-#endif
 
 /* Password entry routine - see password.c in the wiz directory for how
    this is actually implemented in Wiz. If you have an encrypted file,
