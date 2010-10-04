63b60b72203eafbe06460afe5bae359c9edc7bcd
Chris Sutcliffe <ir0nh34d@gmail.com>
2010-10-03 22:03:49 -0400

    Updated to work with more recent eD releases.

--- Source/scripts/hide.rb
+++ Source/scripts/hide.rb
@@ -1,40 +1,23 @@
-#require "Win32API"
-
-#WM_USER=0x0400
-#EMERGE_DISPATCH=WM_USER + 4
-#EMERGE_CORE=1
-#CORE_HIDE=102
-
-#findWindow = Win32API.new("user32", 
-#                          "FindWindow", 
-#                          ['P','P'], 'L')
-#sendMessage = Win32API.new("user32", "SendMessage",
-#                            ['L','L','L','P'], 'L')
-
-#emergeCoreWnd = findWindow.call("emergeCoreClass", 0)
-#sendMessage.call(emergeCoreWnd, EMERGE_DISPATCH, EMERGE_CORE, CORE_HIDE)
-
 require "dl/import"
 
 module Win32
-	extend DL::Importable
-	dlload "user32.dll"
+  extend DL::Importable
+  dlload "user32.dll"
 
-	WM_USER = 0x0400
+  typealias "WPARAM", "UINT"
+  typealias "LPARAM", "UINT"
+  typealias "LPCTSTR", "CHAR *"
 
-	typealias "WPARAM", "UINT"
-	typealias "LPARAM", "UINT"
-	typealias "LPCTSTR", "CHAR *"
-	
-	extern "UINT SendMessage(HWND, UINT, WPARAM, LPARAM)"
-	extern "HWND FindWindow(LPCTSTR, LPCTSTR)"
+  extern "UINT SendMessage(HWND, UINT, WPARAM, LPARAM)"
+  extern "HWND FindWindow(LPCTSTR, LPCTSTR)"
+  extern "UINT RegisterWindowMessage(LPCTSTR)"
 end
 
 module Emerge
-	EMERGE_DISPATCH = Win32::WM_USER + 4
-	EMERGE_CORE = 1
-	CORE_HIDE = 102
+  EMERGE_DISPATCH = Win32::registerWindowMessage("EmergeDispatch")
+  EMERGE_CORE = 1
+  CORE_HIDE = 102
 end
 
-emergeCoreWnd = Win32::findWindow("emergeCoreClass", nil)
+emergeCoreWnd = Win32::findWindow("EmergeDesktopCore", nil)
 Win32::sendMessage(emergeCoreWnd, Emerge::EMERGE_DISPATCH, Emerge::EMERGE_CORE, Emerge::CORE_HIDE)
