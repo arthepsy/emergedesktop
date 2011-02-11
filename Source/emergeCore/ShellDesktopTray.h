ae695a7e02b5a4f34a92a4196dcf9df58e33eb95
Chris Sutcliffe <ir0nh34d@gmail.com>
2011-02-11 07:19:40 -0500

    Code cleanup

--- Source/emergeCore/ShellDesktopTray.h
+++ Source/emergeCore/ShellDesktopTray.h
@@ -7,20 +7,6 @@
 // {213E2DF9-9A14-4328-99B1-6961F9143CE9}
 const IID IID_IShellDesktopTray = {0x213E2DF9,0x9A14,0x4328,{0x99,0xB1,0x69,0x61,0xF9,0x14,0x3C,0xE9}};
 
-//class __declspec(novtable) IShellDesktopTray
-/*class IShellDesktopTray
-{
-	public:
-		virtual HRESULT QueryInterface(IShellDesktopTray * p, REFIID riid, LPVOID * ppvObj)=0;
-		virtual ULONG AddRef(IShellDesktopTray * p)=0;
-		virtual ULONG Release(IShellDesktopTray * p)=0;
-
-		virtual ULONG STDMETHODCALLTYPE GetState()=0;
-		virtual HRESULT STDMETHODCALLTYPE GetTrayWindow(HWND *o)=0;
-		virtual HRESULT STDMETHODCALLTYPE RegisterDesktopWindow(HWND d)=0;
-		virtual HRESULT STDMETHODCALLTYPE SetVar(int p1, ULONG p2)=0;
-};*/
-
 #ifdef __cplusplus
 extern "C"
 {
