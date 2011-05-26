c43b51ce56c961e48af3761c9d7df8a14d711be8
Chris Sutcliffe <ir0nh34d@gmail.com>
2011-05-26 17:39:34 -0400

    Check last error directly after SendMessage then free dataPtr.

--- Source/emergeTrayExplorerHook/emergeTrayExplorerHook.cpp
+++ Source/emergeTrayExplorerHook/emergeTrayExplorerHook.cpp
@@ -1,5 +1,4 @@
 #include "emergeTrayExplorerHook.h"
-#include <stdio.h>
 
 LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -17,9 +16,13 @@ LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
     }
   else if (cwpRetMsg->message == WM_COPYDATA)
     {
-      //WM_COPYDATA is a special message. Windows moves the data referenced by lParam from the sending process to the receiving process, but the data cannot be accessed outside those processes.
-      //This means we can't just directly pass any WM_COPYDATA messages we receive to our message handler; we have to recreate the data within our process space.
-      //I did try directly passing WM_COPYDATA messages, but emergeTray crashed, probably from memory access errors. This works much better.
+      //WM_COPYDATA is a special message. Windows moves the data referenced by
+      //lParam from the sending process to the receiving process, but the data
+      //cannot be accessed outside those processes.  This means we can't just
+      //directly pass any WM_COPYDATA messages we receive to our message
+      //handler; we have to recreate the data within our process space.  I did
+      //try directly passing WM_COPYDATA messages, but emergeTray crashed,
+      //probably from memory access errors. This works much better.
       PCOPYDATASTRUCT receivedData = (PCOPYDATASTRUCT)cwpRetMsg->lParam;
       DWORD dataSize = receivedData->cbData;
       void * dataPtr = malloc(dataSize);
@@ -31,17 +34,23 @@ LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
           sendingData.dwData = receivedData->dwData;
           sendingData.cbData = dataSize;
           sendingData.lpData = dataPtr;
-          SendMessage(trayMsgHandler, cwpRetMsg->message, cwpRetMsg->wParam, (LPARAM)(LPVOID)&sendingData);
+          SendMessage(trayMsgHandler, cwpRetMsg->message, cwpRetMsg->wParam,
+                      (LPARAM)(LPVOID)&sendingData);
+          if ((GetLastError() == ERROR_INVALID_HANDLE) ||
+              (GetLastError() == ERROR_INVALID_WINDOW_HANDLE))
+            trayMsgHandler = NULL; //trayMsgHandler is an invalid handle (the
+                                   //window probably closed), so clear it; we
+                                   //don't want to keep sending messages to
+                                   //nothing
           free(dataPtr);
-          if ((GetLastError() == ERROR_INVALID_HANDLE) || (GetLastError() == ERROR_INVALID_WINDOW_HANDLE))
-            trayMsgHandler = NULL; //trayMsgHandler is an invalid handle (the window probably closed), so clear it; we don't want to keep sending messages to nothing
         }
     }
 
   return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL UNUSED, DWORD fdwReason, LPVOID lpvReserved UNUSED)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL UNUSED, DWORD fdwReason,
+                    LPVOID lpvReserved UNUSED)
 {
   switch (fdwReason)
     {
