3ac88a524f35361bc60dc9fce752c54e8e0c9863
Chris Sutcliffe <ir0nh34d@gmail.com>
2011-11-17 09:59:19 -0500

    Alternate fix for compilation error

--- Source/tinyxml/xmltest.cpp
+++ Source/tinyxml/xmltest.cpp
@@ -941,8 +941,9 @@ int main()
 	// Long filenames crashing STL version
 	{
 		TiXmlDocument doc( "midsummerNightsDreamWithAVeryLongFilenameToConfuseTheStringHandlingRoutines.xml" );
-		bool loadOkay = doc.LoadFile();
-		loadOkay = true;	// get rid of compiler warning.
+    (void)doc.LoadFile();
+		//bool loadOkay = doc.LoadFile();
+		//loadOkay = true;	// get rid of compiler warning.
 		// Won't pass on non-dev systems. Just a "no crash" check.
 		//XmlTest( "Long filename. ", true, loadOkay );
 	}
