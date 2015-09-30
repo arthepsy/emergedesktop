59c5a25a4855c1f053e0ab5fe97b93a9f0464002
Andris Raugulis <moo@arthepsy.eu>
2015-09-30 13:46:31 +0300

    Update rev version script.

--- Source/hgversion.bat
+++ Source/hgversion.bat
@@ -1,14 +1,22 @@
 @echo off
-set GIT_ROOT=C:\Program Files (x86)\Git\bin
+set _OPATH=%PATH%
 
-if not exist "%GIT_ROOT%"  goto ErrNoCB
-set PATH=%GIT_ROOT%;%PATH%
+WHERE git >nul 2>nul
+if %ERRORLEVEL% EQU 0 goto UpdateRev
+
+set GIT_ROOT=C:\Program Files (x86)\Git\bin
+set PATH=%GIT_ROOT%;%_OPATH%
+WHERE git >nul 2>nul
+if %ERRORLEVEL% EQU 0 goto UpdateRev
+
+:ErrNoGit
+echo Error: git was not found.
+goto TheEnd
+
+:UpdateRev
 for /f %%i in ('git rev-list --count HEAD') do set rev=%%i
 echo #define BUILD_VERSION %rev% > %1hgversion.h
 goto TheEnd
 
-:ErrNoGit
-echo Error: Git root folder not found. Adjust batch file accordingly
-goto TheEnd
-
 :TheEnd
+set PATH=%_OPATH%
